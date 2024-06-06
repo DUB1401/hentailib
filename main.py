cb354fe6d620a3e9bc2615c4a5d869920552a13b
DUB1401 <vlad.milosta@outlook.com>
2024-06-06 11:09:11 +0300

    Project upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from Parsers.mangalib.main import VERSION, STRUCT
+from Parsers.mangalib.main import Parser
+
+import Parsers.mangalib.main as main 
+
+#==========================================================================================#
+# >>>>> ОПРЕДЕЛЕНИЯ <<<<< #
+#==========================================================================================#
+
+NAME = "hentailib"
+REPOS = "https://github.com/DUB1401/hentailib"
+SITE = "hentailib.me"
+main.NAME = NAME
+main.SITE = SITE
